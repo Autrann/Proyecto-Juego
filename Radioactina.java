fb7b8e222efa33f273919ae13ea6e6d1932572e6
Autrann <luis.autran2@gmail.com>
2022-06-02 01:10:42 -0500

    Proyecto terminado

---
+++ Radioactina.java
@@ -0,0 +1,67 @@
+import greenfoot.*;
+
+public class Radioactina extends items
+{
+    private GreenfootImage Radioactina;
+    public boolean Up = false;
+    private int desplaza=1;
+    public int nivelRadio=0;
+    public int i=0;
+    public int nivel=1;
+    public int flag;
+    public int cont=1;
+    
+    GreenfootSound coin = new GreenfootSound("Radioactina.mp3");
+    
+    public Radioactina()
+    {
+       this(35,55);
+       Radioactina = new GreenfootImage("Radiactina☢.png");
+    }
+    public Radioactina(int width, int height){
+      GreenfootImage image = getImage();
+      image.scale(width, height);
+      setImage(image);
+ 
+    }
+    
+    public void act() 
+    {  
+         checkForCollisions();
+    }
+    
+    
+    public void checkForCollisions() 
+    {       
+       Actor baker = getOneIntersectingObject(Baker.class);
+       
+       if(baker != null)
+       {
+            Baker.score++;
+            getWorld().showText("Score = " + Baker.score, 100, 100);
+            
+               if(getWorld() instanceof World1)
+               {
+                   World1 mundo = (World1)getWorld();
+                   RadBar Rad = mundo.getRadBar();
+                   Rad.PlusHealth();
+               }
+               if(getWorld() instanceof World2)
+               {
+                   World2 mundo = (World2)getWorld();
+                   RadBar Rad = mundo.getRadBar();
+                   Rad.PlusHealth();
+               }
+               if(getWorld() instanceof World3)
+               {
+                   World3 mundo = (World3)getWorld();
+                   RadBar Rad = mundo.getRadBar();
+                   Rad.PlusHealth();
+               }
+               getWorld().removeObject(this);
+               coin.play();      
+       }
+    
+    }
+   
+}
