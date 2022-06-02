ca9ae399c1bd90e855b7e6c8e8c4b4bd37d2fe44
Autrann <luis.autran2@gmail.com>
2022-06-02 01:05:55 -0500

    Limpia

--- HUD.java
+++
@@ -1,9 +0,0 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-public class HUD extends Actor
-{
-    public void act() 
-    {
-        // Add your action code here.
-    }    
-}
